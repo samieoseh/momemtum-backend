11d20d96e3ec161cfbeb009499bd85b927cb8cb0
Samuel Oseh <samieoseh@gmail.com>
2025-03-28 10:57:31 +0100

    ST-4 user can now send emails

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { SignupDto } from './dto/signup-dto';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login-dto';
+import { ForgetPasswordDto } from './dto/forgot-password-dto';
 
 @Controller('auth')
 export class AuthController {
@@ -13,8 +14,18 @@ export class AuthController {
   }
 
   @Post('/login')
+  @HttpCode(200)
   async login(@Body() loginDto: LoginDto) {
     const user = await this.authService.login(loginDto);
     return { user, message: 'User login successfully' };
+  }
+
+  @Post('/forgot-password')
+  @HttpCode(200)
+  async forgotPassword(@Body() forgotPasswordDto: ForgetPasswordDto) {
+    await this.authService.forgotPassword(forgotPasswordDto);
+    return {
+      message: 'A password reset link have been sent to the provided email',
+    };
   }
 }
