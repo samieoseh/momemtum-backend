c0bb52d943f51a0c297dba60145148941aaaec13
Samuel Oseh <samieoseh@gmail.com>
2025-03-29 19:43:31 +0100

    updated tenant.service to use environment

--- src/tenant/tenant.service.ts
+++ src/tenant/tenant.service.ts
@@ -2,13 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Tenant } from './domain/tenant.schema';
+import { env } from 'process';
 
 @Injectable()
 export class TenantService {
   constructor(@InjectModel(Tenant.name) private tenantModel: Model<Tenant>) {}
 
   async createTenant(companyName: string): Promise<Tenant> {
-    const databaseUri = `${process.env.COMPANY_URI}/${companyName}_db`;
+    let databaseUri: string;
+    if (process.env.NODE_ENV === 'development') {
+      databaseUri = `${process.env.COMPANY_URI}/${companyName}_db`;
+    } else {
+      databaseUri = `${process.env.COMPANY_URI}/${companyName}_db?retryWrites=true&w=majority&appName=Cluster0`;
+    }
 
     const newTenant = new this.tenantModel({ companyName, databaseUri });
     console.log({ newTenant });
