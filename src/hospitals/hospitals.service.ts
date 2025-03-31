37a55bba4b75854a31b0496dcd9305c82c51d516
Samuel Oseh <samieoseh@gmail.com>
2025-03-31 16:59:03 +0100

    user can now delete hospital

--- src/hospitals/hospitals.service.ts
+++ src/hospitals/hospitals.service.ts
@@ -3,11 +3,17 @@ import { HospitalUpdateDto } from './dto/hospital-update-dto';
 import { Model } from 'mongoose';
 import { Hospital } from './domain/hospital.schema';
 import { InjectModel } from '@nestjs/mongoose';
+import { TenantService } from '../tenant/tenant.service';
+import { Tenant } from '../tenant/domain/tenant.schema';
+import { TenantDatabaseService } from '../tenant/tenant.database.service';
 
 @Injectable()
 export class HospitalsService {
   constructor(
     @InjectModel(Hospital.name) private hospitalModel: Model<Hospital>,
+    @InjectModel(Tenant.name) private tenantModel: Model<Tenant>,
+    private readonly tenantService: TenantService,
+    private readonly tenantDatabaseService: TenantDatabaseService,
   ) {}
   async findHospital(id: string) {
     return await this.hospitalModel.findOne({ _id: id });
@@ -34,5 +40,35 @@ export class HospitalsService {
     );
 
     return updatedHospital;
+  }
+
+  async deleteHospital(tenantId: string, hospitalId: string) {
+    // drop the hospital
+    const hospital = await this.findHospital(hospitalId);
+
+    if (!hospital) {
+      throw new NotFoundException('Hospital not found');
+    }
+
+    await this.hospitalModel.findByIdAndDelete(hospitalId);
+
+    // drop the tenant
+    const tenant = await this.tenantService.findByTenantId(tenantId);
+
+    if (!tenant) {
+      throw new NotFoundException('Tenant not found');
+    }
+
+    await this.tenantModel.findByIdAndDelete(tenantId);
+
+    const tenantConnection = await this.tenantDatabaseService.getConnection(
+      tenant.databaseUri,
+    );
+
+    console.log({ tenantConnection });
+
+    await tenantConnection?.dropDatabase();
+
+    return { tenantId, hospitalId };
   }
 }
